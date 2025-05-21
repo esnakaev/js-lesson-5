f3488c130d503192f034315e8ccfece4eb812531
esnakaev <deniesnakaev95@gmail.com>
2025-05-21 13:59:26 +0300

    start

---
+++ script.js
@@ -0,0 +1,12 @@
+const myName = "Дени";
+const programmingLanguage = "JavaScript";
+const courseCreatorName = "Максим";
+const reasonText = "Работа будующего";
+const numberOfMonth = "Первый месяц"; 
+
+let text = `Всем привет! Меня зовут, ${myName}. Сейчас я изучаю язык программирования
+${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}.
+Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал
+${programmingLanguage} ${numberOfMonth}. Я уверен, что пройду данный курс до
+конца!`;
+console.log(text); 
